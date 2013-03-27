b3d1fb34543d1373d7fb90d75174f31bd4a0f599
Grant Williamson <grant.williamson@utas.edu.au>
2013-03-27 20:38:09 +1100

    Modified classes to make **push** a method of the Scene class, rather than the object classes.

--- povray.r
+++ povray.r
@@ -1,8 +1,7 @@
 ## R-POVRAY
 
-povpath=
-
-Script=function(){
+# Scene class
+Scene=function(){
   self=list(
     contents=c(),
     add=function(text){
@@ -14,18 +13,22 @@ Script=function(){
         cat(line,file=f,sep="\n")
       }
        close(f)
+    },
+    push=function(object){
+      self$contents=c(self$contents,object$format())
     }
     )
   self <- list2env(self)
   class(self) <- "Script"
   return(self) 
 }  
-  
+
+# Light class
 Light=function(location=c(x,y,z),colour=c(1,1,1)){
   self=list(
     location=location,
     colour=colour,
-    add=function(script){
+    format=function(){
       x=self$location[1]
       y=self$location[2]
       z=self$location[3]
@@ -33,7 +36,7 @@ Light=function(location=c(x,y,z),colour=c(1,1,1)){
       g=self$colour[2]
       b=self$colour[3]
       text=paste("light_source {  <",x,",",y,",",z,"> color <",r,",",g,",",b,">}",sep="")
-      script$add(text)
+      text
     }
   )
   self <- list2env(self)
@@ -41,12 +44,12 @@ Light=function(location=c(x,y,z),colour=c(1,1,1)){
   return(self) 
 }
 
-
+# Camera class
 Camera=function(location=c(x,y,z),looking=c(x2,y2,z2)){
   self=list(
     location=location,
     looking=looking,
-    add=function(script){
+    format=function(){
       x=self$location[1]
       y=self$location[2]
       z=self$location[3]
@@ -54,10 +57,11 @@ Camera=function(location=c(x,y,z),looking=c(x2,y2,z2)){
       y2=self$looking[2]
       z2=self$looking[3]
       text=paste("camera { location <",x,",",y,",",z,"> look_at <",x2,",",y2,",",z2,"> right x*image_width/image_height}",sep="")
-      script$add(text)
+      text
     }
   )
   self <- list2env(self)
   class(self) <- "Camera"
   return(self) 
 }
+
