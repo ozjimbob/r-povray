dac3222fe4922f262caf35a915d061f06d3bc21d
Grant Williamson <grant.williamson@utas.edu.au>
2013-03-30 20:16:04 +1100

    Improved test render file.

--- test.r
+++ test.r
@@ -33,6 +33,11 @@ for(idx in 1:20){
   sc$push(cyl)
 }
 
+# Box for level
+half_grey=Colour(.6,.6,.6,.6)
+b=Box(c(-20,-0.1,-10),c(20,0.1,10),col=half_grey)
+sc$push(b)
+
 # Push in the lights and camera as well, save and render the scene.
 sc$push(l1)
 sc$push(l2)
