9a876106e646adbdc4be28ae7a6947b0e08a54ef
Grant Williamson <grant.williamson@utas.edu.au>
2013-03-31 15:54:24 +1100

    Added predefined texture and interior file.

---
+++ R/textures.r
@@ -0,0 +1,4 @@
+# Define some useful textures.
+
+glass=Texture(" pigment { color rgbf<0.7, 1.0, 0.7, 0.7> } finish {specular 1 roughness 0.001 ambient 0 diffuse 0 reflection 0.1}  ")
+i_glass=Interior("ior 1.5")
